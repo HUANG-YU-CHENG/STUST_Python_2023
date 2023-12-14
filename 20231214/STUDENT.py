c1a97fe824e7e93c08aeb678e7719fbe3fe56cb1
HUANG-YU-CHENG <ve8132966@gmail.com>
2023-12-14 22:10:49 +0800

    20231214晚上10:10修改作業
    將命名為session的地方改為course

--- 20231214/STUDENT.py
+++ 20231214/STUDENT.py
@@ -1,25 +1,24 @@
 class student:
     def __init__(self,name,student_ID):
-        session = []
-        self.session = session
+        self.course = []
         self._name = name
         self._student_ID = student_ID
     @property
-    def student_session(self):
-        return self.session
-    @student_session.setter
-    def add_session(self,new_session):
-        self.session.append(new_session)
-    @student_session.deleter
-    def withdraw_session(self,del_session):
-        del self.session[del_session]
+    def student_course(self):
+        return self.course
+    @student_course.setter
+    def add_course(self,new_course):
+        self.course.append(new_course)
+    @student_course.deleter
+    def withdraw_course(self,del_course):
+        del self.course[del_course]
     def print_data(self):
-        print(f"{self._name}目前選到的課程有{self.session}")
+        print(f"{self._name}目前選到的課程有{self.course}")
 def main():
     obj1 = student("huang","4b0g0109")
-    obj1.session = ["test","test2","test3"]
-    obj1.session.append("obj3") 
-    del obj1.session[1]
+    obj1.course = ["test","test2","test3"]
+    obj1.course.append("obj3") 
+    del obj1.course[1]
     obj1.print_data()
 if __name__ == '__main__':
     main()       
