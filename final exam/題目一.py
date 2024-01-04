6bb9e42e16951512ce57d4b652702ddc328a8feb
HUANG-YU-CHENG <ve8132966@gmail.com>
2024-01-04 10:36:46 +0800

    期末考考試題目ver2
    期末考

--- final exam/題目一.py
+++ final exam/題目一.py
@@ -1,18 +1,38 @@
 class fried_chicken:
-    def __init__(self,meat,flour,fried_time,flavor,number):
+    def __init__(self,meat,flour,fried_time,flavor,number):#建構子分別為肉的種類 麵粉選擇 油炸時間 口味 份量
         self.meat = meat
         self.flour = flour
         self.fried_time = fried_time
         self.flavor = flavor
         self.number = number
+    #選擇口味
     def choose_flavor(self):
-        return print(self.flavor)
+        print(f"你選擇的口味是{self.flavor}口味")
+    #準備時間
     def prepare_fried_time(self):
-        return print(self.fried_time)
+        print(f"準備時間需要{self.fried_time}")
+    #選擇份量
     def calling_serving(self):
-        return print(self.number)
+        print(f"選擇份量為{self.number}")
 def main():
-    obj1 = fried_chicken("chicken","corn,""vanila","2m30s","a serving")
+    obj1 = fried_chicken("雞胸肉","玉米粉","2m30s","香草","一份")
     obj1.choose_flavor()
     obj1.prepare_fried_time()
     obj1.calling_serving()
+
+    obj2 = fried_chicken("烏骨雞","麵粉","3m30s","黑胡椒","三份")
+    obj2.choose_flavor()
+    obj2.prepare_fried_time()
+    obj2.calling_serving()
+
+    obj3 = fried_chicken("雞腿","玉薯粉","2m00s","韓式炸雞","兩份")
+    obj3.choose_flavor()
+    obj3.prepare_fried_time()
+    obj3.calling_serving()
+
+    obj4 = fried_chicken("雞胸肉","麵粉","3m00s","黑胡椒","九份")
+    obj4.choose_flavor()
+    obj4.prepare_fried_time()
+    obj4.calling_serving()
+if __name__ == '__main__':
+    main()
--- final exam/題目二.py
+++ final exam/題目二.py
@@ -1,13 +1,50 @@
 class employer:
     def __init__(self,name,seniority,work_time):
+        self.minimum_wage = 173 #基本時新
         self.name = name
         self.seniority = seniority
         self.work_time = work_time
+    #查詢名字
+    def check_name(self):
+        print(f"員工名為{self.name}")
+    #查詢年資
+    def check_seniority(self):
+        print(f"該員工年薪有{self.seniority}")
+    #查詢工時
+    def check_work_time(self):
+        print(f"該員工工時有{self.work_time}")
+    #計算月薪
+    def count_salary(self):
+        salary = self.work_time * self.minimum_wage #月薪計算
+        print(f"該員工月薪為{salary}")
+    #增加工時
+    def add_work_time_hour(self,addtional_work_time):
+        print(f"該員工加班時間為{addtional_work_time},總時{self.work_time + addtional_work_time}")
+    #增加年資
+    def add_seniority(self,addtional_seniority):
+        print(f"該員工額外年資為{addtional_seniority},總年資為{self.seniority + addtional_seniority}")
+        
+        
 class drinks:
-    def __init__(self,price):
+    def __init__(self,price,name,sweet):
         self.price = price
+        self.name = name
+        self.sweet = sweet
+    #更換名稱
+    def change_name(self,swap_name):
+        self.name == swap_name
+        print(self.name)
+    #調整甜度
+    def change_sweet(self,swap_sweet):
+        self.sweet == swap_sweet
+        print(self.sweet)
+    #調整價錢
+    def change_price(self,swap_price):
+        self.price == swap_price
+        print(self.price)
 class cold(drinks):
     def __init__(self,name,ice,sweet):
+        super().__init__(self.price)
         self.name = name
         self.ice = ice
         self.sweet = sweet
@@ -15,3 +52,15 @@ class hot(drinks):
     def __init__(self,name,sweet):
         self.name = name
         self.sweet = sweet
+def main():
+    Jack = employer("Jack",0.8,120)
+    Jack.check_name()
+    Jack.check_seniority()
+    Jack.check_work_time()
+    Jeff = employer("Jeff",2,130)
+    Jeff.count_salary()
+    Jeff.add_seniority(1)
+    Jacky = employer("Jacky",0.5,140)
+    Jacky.add_work_time_hour(20)
+if __name__ == "__main__":
+    main()
